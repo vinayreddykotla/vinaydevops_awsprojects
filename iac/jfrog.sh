07f5082388a20eb08db572d79964927cbbf3fcd2
bob-at-215816675325 <bob-at-215816675325>
2023-11-18 11:37:32 +0530

    pom.xml

--- iac/jfrog.sh
+++ iac/jfrog.sh
@@ -5,13 +5,13 @@ sudo apt-get update
 sudo apt-get install vim curl elinks unzip wget tree git -y 
 sudo apt-get install openjdk-17-jdk -y 
 sudo cp -pvr /etc/environment "/etc/environment_$(date +%F_%R)"
-echo "JAVA_HOME=/usr/lib/jvm/java-17-amazon-corretto.x86_64/" >> /etc/environment
+#echo "JAVA_HOME=/usr/lib/jvm/java-17-amazon-corretto.x86_64/" >> /etc/environment
 echo "JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64/" >> /etc/environment 
 source /etc/environment
 cd /opt/
 sudo wget  https://releases.jfrog.io/artifactory/bintray-artifactory/org/artifactory/oss/jfrog-artifactory-oss/[RELEASE]/jfrog-artifactory-oss-[RELEASE]-linux.tar.gz  
 tar xvzf jfrog-artifactory-oss-\[RELEASE\]-linux.tar.gz 
-mv artifactory-oss-7.55.6/ jfrog
+mv artifactory-oss-* jfrog
 sudo cp -pvr /etc/environment "/etc/environment_$(date +%F_%R)"
 echo "JFROG_HOME=/opt/jfrog" >> /etc/environment
 cd /opt/jfrog/app/bin/ 
